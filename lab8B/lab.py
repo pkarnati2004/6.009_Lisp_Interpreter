20ba34dd3d87aa3a37a70eda88e22ed44a652420
pkarnati2004 <pkarnati@mit.edu>
2017-11-19 21:15:12 -0500

    clean up code

--- lab8B/lab.py
+++ lab8B/lab.py
@@ -158,41 +158,59 @@ class LinkedList:
         # copy first element to this function mapped to list elt
         new = LinkedList(evaluate_function(function, [self.elt]))
         prev = new
+        # while next exists, find next
         while self.next != None:
             self = self.next
-            # prev.next = LinkedList(function([self.elt]))
+            # set next pointer to a new list with this function applied to elt
             prev.next = LinkedList(evaluate_function(function, [self.elt]))
             prev = prev.next
+        # return new list
         return new
     def filter(self, function):
+        """
+        filter elements of list by a function
+        """
+        # if the function applies, create a new list
         if evaluate_function(function, [self.elt]):
             new = LinkedList(self.elt)
+        # else this is none
         else: new = None
         prev = new
+        # while next exists, find next of list
         while self.next != None:
             self = self.next
-            # print(self.elt)
+            # create a new list with this function applied
             current = LinkedList(self.elt) if evaluate_function(function, [self.elt]) else None
+            # if prev value was none, set prev to this current
+            # if new is none, set new to prev
             if prev is None:
                 prev = current
                 if new is None: new = prev
-                # prev.print()
-                # prev = prev.next
+            # if current value is none, don't change prev
             elif current is None:
                 prev = prev
+            # get last of prev and current to this list
             else:
                 last = prev.get_last()
                 last.add_next(current)
                 prev = prev.next
-        # new.print()
+        # return new
         return new
     def reduce(self, function, init):
+        """
+        reduce a list given a function and initial value
+        """
+        # get first val
         val = self.elt
+        # apply function to first val and initial value
         result = evaluate_function(function, [init, val])
+        # while next exists, find next
         while self.next != None:
             self = self.next
             val = self.elt
+            # change result by applying function to this val and result
             result = evaluate_function(function, [result, val])
+        # return result
         return result
         
 
@@ -371,43 +389,46 @@ def divide(l):
         return l[0]/multiply(l[1:])
 
 def eq(args):
+    # if all are equal in args, return true
     return all(x == args[0] for x in args)
 
 def gtr(args):
-    # last = args[0]
-    # for n in args[1:]:
-    #     if n < last:
-    #         return False
-    #     last = n
-    # return True
+    # make sure all elements are greater than next
     return all(last > n for last, n in zip(args, args[1:]))
-    # return not any(last >= n for last, x in zip(args, args[1:]))
 
 def lst(args):
+    # make sure all elements are less than next
     return all(last < n for last, n in zip(args, args[1:]))
 
 def gtreq(args):
+    # make sure any elements are not less than next
     return not any(last < n for last, n in zip(args, args[1:]))
 
 def lsteq(args):
+    # make sure any elements are not greater than next
     return not any(last > n for last, n in zip(args, args[1:]))
 
 def AND(args, env):
     # return all(evaluate(a, env) for a in args)
+    # if any of the args are not True, return False
     for a in args:
         if not evaluate(a, env):
             return False
+    # return true if all pass
     return True
 
 def OR(args, env):
     # return any(a for a in args)
+    # if any of the args are True, return True
     for a in args:
         if evaluate(a, env):
             return True
+    # return false if none pass
     return False
 
 def NOT(args, env):
     # return not args[0]
+    # return not of this value
     return not evaluate(args[0], env)
 
 carlae_builtins = {
@@ -443,12 +464,20 @@ def get_env_part_2(env, symbol):
     raise EvaluationError
 
 def get_env(env, symbol):
+    """
+    helper to return env of symbol starting with env
+    """
+    # to not mutate env
     new_env = env
+    # while env is not None
     while new_env != None:
+        # if symbol exists in this env, return this env
         if symbol in new_env.env:
             return new_env
+        # set env to parent
         else:
             new_env = new_env.parent
+    # if not found raise Error
     raise EvaluationError
 
 def evaluate_function(keyword, evl):
@@ -538,134 +567,166 @@ def evaluate_helper(tree, env = None):
                 # env.env[var] = val
                 # print(env)
                 return val
+        # conditionals
         elif keyword == 'if':
             # print('hello i am here')
+            # evaluate the conditional 
             conditional = evaluate_helper(tree[1], env)
             true_exp, false_exp = tree[2], tree[3]
+            # if conditional is true, evaluate true exp
             if conditional:
                 return evaluate_helper(true_exp, env)
+            # else evaluate false exp
             else:
                 return evaluate_helper(false_exp, env)
+        # and, or, not
         elif keyword == 'and' or keyword == 'or' or keyword == 'not':
+            # find all of the conditionals in the list
             all_conditionals = tree[1:]
+            # evaluate these conditionals
             function = evaluate_helper(keyword, env)
+            # apply function (and, not, or) to conditionals
             return function(all_conditionals, env)
+        # list functions
         elif keyword == 'list':
+            # get args for list
             args = tree[1:]
             if args == []:
-                ls = LinkedList(None)
-                return ls
+                # if there are no args, return a list of None
+                return LinkedList(None)
             else:
-                # print(args[0])
-                # print(evaluate_helper(args[0], env))
-                # ls = LinkedList(args[0])
+                # make list of first element
                 ls = LinkedList(evaluate_helper(args[0], env))
                 prev = ls
-                # print(args[0])
+                # for every other value in args
                 for val in args[1:]:
-                    # print(val)
-                    # print(evaluate_helper(val, env))
+                    # evaluate the value, create new linkedlist object
                     val = evaluate_helper(val, env)
                     new = LinkedList(val)
+                    # set this new linkedlist as next of prev, change prev
                     prev.add_next(new)
                     prev = new
-                # ls.print()
-                # ls = ls.remove_none()
-                # print('after remove none')
-                # ls.print()
+                # return list
                 return ls
         elif keyword == 'car' or keyword == 'cdr':
+            # evaluate the list in question
             ls = evaluate_helper(tree[1], env)
+            # if it's not a list, raise error
             if type(ls) != LinkedList:
                 raise EvaluationError('This is not a list')
+            # if its empty raise error
             if ls.elt is None:
                 raise EvaluationError('This is an empty list')
-            # print(tree[1])
+            # if its car, apply car
             if keyword == 'car':
-                # ls.print()
-                # return ls.elt
                 return ls.car()
+            # if its cdr, apply cdr
             else:
-                # ls = ls.next
-                # return ls
                 return ls.cdr()
         elif keyword == 'length':
-            # if tree[1][0] != 'list':
-            #     # tree[1] = ['list'] + tree[1]
-            #     tree[1] = ['list', tree[1]]
-            # print(tree[1])
+            # evaluate the function at hand
             ls = evaluate_helper(tree[1], env)
-            # ls.print()
+            # if its not a list, raise error
             if type(ls) != LinkedList:
                 raise EvaluationError('This is not a list')
+            # if its empty, return 0
             if ls.elt is None:
                 return 0
-            # print(ls.length())
+            # apply length to list
             return ls.length()
         elif keyword == 'elt-at-index':
+            # evaluate list, get index
             ls = evaluate_helper(tree[1], env)
             index = tree[2]
+            # if its not a list, raise error
             if type(ls) != LinkedList:
                 raise EvaluationError('This is not a list')
+            # if list is empty, raise error
             if ls.elt is None:
                 raise EvaluationError('This is an empty list')
+            # if index is greater than length, raise error
             if index >= ls.length():
                 raise EvaluationError('Index out of bounds')
-            # print(ls.val_at_index(index))
+            # apply val_at_index to list
             return ls.val_at_index(index)
         elif keyword == 'concat':
+            # if there are no args, return None
             if len(tree) == 1:
                 return None
-            # if len(tree[1:]) == 1:
-            #     ls = evaluate_helper(tree[1], env)
-            #     return ls.copy()
+            # get first list
             ls = evaluate_helper(tree[1], env)
             start = ls
+            # for rest of args
             for t in tree[2:]:
+                # evaluate value
                 new = evaluate_helper(t, env)
+                # if start was None, set start to new
                 if start is None:
                     start = new
+                # concatenate lists (call __add__)
                 else:
                     start = start + new
-            # start.print()
+            # return start
             return start
         elif keyword == 'map':
+            # evaluate function and list
             function = evaluate_helper(tree[1], env)
             ls = evaluate_helper(tree[2], env)
+            # apply map to list
             new = ls.map(function)
-            # new.print()
+            # return new copy
             return new
         elif keyword == 'filter':
+            # evaluate function and list
             function = evaluate_helper(tree[1], env)
             ls = evaluate_helper(tree[2], env)
+            # apply filter to list
             new = ls.filter(function)
-            # new.print()
+            # return new copy
             return new
         elif keyword == 'reduce':
+            # evaluate function, list, and init
             function = evaluate_helper(tree[1], env)
             ls = evaluate_helper(tree[2], env)
             init = evaluate_helper(tree[3], env)
+            # apply reduce to list
             return ls.reduce(function, init)
+        # for begin
         elif keyword == 'begin':
+            # get args and last index
             args = tree[1:][:]
             last = len(args) - 1
+            # iterate through args and indices
             for index, arg in enumerate(args):
+                # if at end, return this evaluated
                 if index == last:
                     return evaluate_helper(arg, env)
+                # evaluate current args
                 evaluate_helper(arg, env)
+        # for let
         elif keyword == 'let':
+            # get args, body, and create new env with this as parent
             args = tree[1]
             body = tree[2]
             this_env = Environment(env)
+            # for all args
             for arg in args:
+                # get var and val
                 var, val = arg
+                # evaluate value 
                 val = evaluate_helper(val, env)
+                # add this to new current env
                 this_env.add_val(var, val)
+            # return the body evalulated in this env
             return evaluate_helper(body, this_env)
+        # for set!
         elif keyword == 'set!':
+            # get var and expr
             var = tree[1]
             expr = evaluate_helper(tree[2], env)
+            # find env of var
             new_env = get_env(env, var)
+            # change env
             new_env.add_val(var, expr)
             return expr
         # if this is a function
@@ -736,22 +797,28 @@ def result_and_env(tree, env = None):
     return (result, env)
 
 def evaluate_file(filename, env=None):
+    """
+    function to evaluate files
+    """
+    # open the file and join all lines
     f = open(filename, 'r')
     exp = ' '.join(line.strip() for line in f)
     f.close()
+    # evaluate and return
     result = evaluate(parse(tokenize(exp)), env)
     return result
 
 x = "(begin (define (foo bar) (lambda (x y) (- bar x y))) (define bar 7) (define something (foo 6)) (list (something 2 3) ((foo 9) 8 7)))"
 
-def REPL():
+def REPL(env = None):
     """
     Read, Evaluate, Print Loop for testing code
     """
     # grab input
     inp = input('inpt > ')
     # while not quit
-    env = create_new_env(None)
+    if env is None:
+        env = create_new_env(None)
     while inp != 'QUIT' and inp != 'quit':
         # try to evaluate input
         try:
@@ -768,4 +835,9 @@ if __name__ == '__main__':
     # code in this block will only be executed if lab.py is the main file being
     # run (not when this module is imported)
     # pass
-    REPL()
+    env = create_new_env(None)
+    args = sys.argv
+    for arg in args[1:]:
+        # evaluate(parse(tokenize(arg)), env)
+        evaluate_file(arg, env)
+    REPL(env)
